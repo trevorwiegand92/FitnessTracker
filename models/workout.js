4391f95348e2b089774ae2ec583c7a3a1dd9a015
Trevor Wiegand <trevorwiegand92@gmail.com>
2021-10-25 11:53:36 -0500

    maid sure all the w's in workout are the same

--- models/workout.js
+++ models/workout.js
@@ -2,37 +2,39 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Exercise type is required.",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
     },
-    exercises: [{
-        type: {
-            type: String,
-            trim: true,
-            required: "Exercise type is required."
-        },
-        name: {
-            type: String,
-            trim: true,
-            required: true,
-        },
-        weight: {
-            type: Number,
-        },
-        sets: {
-            type: Number,
-        },
-        reps: {
-            type: Number,
-        },
-        duration: {
-            type: Number,
-        },
-        distance: {
-            type: Number,
-        }
-    }],
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
